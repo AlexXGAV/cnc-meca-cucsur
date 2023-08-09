eb668c12cb0400052e84f9221fd5ce4aa89deb1b
Alex Garcia <alexgarciavillalvazo@gmail.com>
2023-08-08 22:52:24 -0600

    change languages settings

--- cnc_meca_cucsur/settings.py
+++ cnc_meca_cucsur/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'cnc_meca_cucsur.urls'
@@ -110,13 +111,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-MX'
-
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+LOCALE_PATHS = (
+    os.path.join(PROJECT_PATH, '../locale'),
+)
+
+LANGUAGES = (
+    ('es-MX', 'Spanish'),
+    ('en-us', 'English'),
+)
+
+LANGUAGE_CODE = 'es-MX'
 
 
 # Static files (CSS, JavaScript, Images)
